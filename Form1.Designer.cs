698077a96b2c73528dce54213ae2f2fe606db985
avanysouza <avanysouza@gmail.com>
2024-02-15 00:40:57 +0000

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace CalculoIMC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblPeso = new System.Windows.Forms.Label();
+            this.lblAltura = new System.Windows.Forms.Label();
+            this.txtPeso = new System.Windows.Forms.TextBox();
+            this.txtAltura = new System.Windows.Forms.TextBox();
+            this.btnCalculoIMC = new System.Windows.Forms.Button();
+            this.lblIMC = new System.Windows.Forms.Label();
+            this.txtIMC = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtClassificacao = new System.Windows.Forms.TextBox();
+            this.lblClassificacao = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.lblPaciente = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblPeso
+            // 
+            this.lblPeso.AutoSize = true;
+            this.lblPeso.Location = new System.Drawing.Point(438, 170);
+            this.lblPeso.Name = "lblPeso";
+            this.lblPeso.Size = new System.Drawing.Size(39, 16);
+            this.lblPeso.TabIndex = 0;
+            this.lblPeso.Text = "Peso";
+            this.lblPeso.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblAltura
+            // 
+            this.lblAltura.AutoSize = true;
+            this.lblAltura.Location = new System.Drawing.Point(438, 221);
+            this.lblAltura.Name = "lblAltura";
+            this.lblAltura.Size = new System.Drawing.Size(41, 16);
+            this.lblAltura.TabIndex = 1;
+            this.lblAltura.Text = "Altura";
+            this.lblAltura.Click += new System.EventHandler(this.lblAltura_Click);
+            // 
+            // txtPeso
+            // 
+            this.txtPeso.Location = new System.Drawing.Point(546, 164);
+            this.txtPeso.Name = "txtPeso";
+            this.txtPeso.Size = new System.Drawing.Size(126, 22);
+            this.txtPeso.TabIndex = 2;
+            this.txtPeso.TextChanged += new System.EventHandler(this.txtPeso_TextChanged);
+            // 
+            // txtAltura
+            // 
+            this.txtAltura.Location = new System.Drawing.Point(546, 221);
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(126, 22);
+            this.txtAltura.TabIndex = 3;
+            this.txtAltura.TextChanged += new System.EventHandler(this.txtAltura_TextChanged);
+            // 
+            // btnCalculoIMC
+            // 
+            this.btnCalculoIMC.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnCalculoIMC.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculoIMC.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnCalculoIMC.Location = new System.Drawing.Point(470, 265);
+            this.btnCalculoIMC.Name = "btnCalculoIMC";
+            this.btnCalculoIMC.Size = new System.Drawing.Size(174, 37);
+            this.btnCalculoIMC.TabIndex = 4;
+            this.btnCalculoIMC.Text = "Calcular IMC";
+            this.btnCalculoIMC.UseVisualStyleBackColor = false;
+            this.btnCalculoIMC.Click += new System.EventHandler(this.btnCalculoIMC_Click);
+            // 
+            // lblIMC
+            // 
+            this.lblIMC.AutoSize = true;
+            this.lblIMC.Location = new System.Drawing.Point(438, 334);
+            this.lblIMC.Name = "lblIMC";
+            this.lblIMC.Size = new System.Drawing.Size(30, 16);
+            this.lblIMC.TabIndex = 5;
+            this.lblIMC.Text = "IMC";
+            this.lblIMC.Click += new System.EventHandler(this.lblIMC_Click);
+            // 
+            // txtIMC
+            // 
+            this.txtIMC.Location = new System.Drawing.Point(546, 331);
+            this.txtIMC.Name = "txtIMC";
+            this.txtIMC.Size = new System.Drawing.Size(126, 22);
+            this.txtIMC.TabIndex = 6;
+            this.txtIMC.TextChanged += new System.EventHandler(this.txtIMC_TextChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(72, 114);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(298, 272);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.textBox1.Location = new System.Drawing.Point(262, 33);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(331, 45);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.Text = "Calculadora de IMC";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtClassificacao
+            // 
+            this.txtClassificacao.BackColor = System.Drawing.SystemColors.Menu;
+            this.txtClassificacao.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.txtClassificacao.Location = new System.Drawing.Point(546, 378);
+            this.txtClassificacao.Multiline = true;
+            this.txtClassificacao.Name = "txtClassificacao";
+            this.txtClassificacao.Size = new System.Drawing.Size(246, 62);
+            this.txtClassificacao.TabIndex = 12;
+            this.txtClassificacao.TextChanged += new System.EventHandler(this.txtClassificacao_TextChanged);
+            // 
+            // lblClassificacao
+            // 
+            this.lblClassificacao.AutoSize = true;
+            this.lblClassificacao.Location = new System.Drawing.Point(438, 378);
+            this.lblClassificacao.Name = "lblClassificacao";
+            this.lblClassificacao.Size = new System.Drawing.Size(88, 16);
+            this.lblClassificacao.TabIndex = 11;
+            this.lblClassificacao.Text = "Classificação";
+            this.lblClassificacao.Click += new System.EventHandler(this.lblClassificacao_Click);
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(546, 114);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(126, 22);
+            this.txtNome.TabIndex = 14;
+            // 
+            // lblPaciente
+            // 
+            this.lblPaciente.AutoSize = true;
+            this.lblPaciente.Location = new System.Drawing.Point(438, 120);
+            this.lblPaciente.Name = "lblPaciente";
+            this.lblPaciente.Size = new System.Drawing.Size(60, 16);
+            this.lblPaciente.TabIndex = 13;
+            this.lblPaciente.Text = "Paciente";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(828, 500);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.lblPaciente);
+            this.Controls.Add(this.txtClassificacao);
+            this.Controls.Add(this.lblClassificacao);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtIMC);
+            this.Controls.Add(this.lblIMC);
+            this.Controls.Add(this.btnCalculoIMC);
+            this.Controls.Add(this.txtAltura);
+            this.Controls.Add(this.txtPeso);
+            this.Controls.Add(this.lblAltura);
+            this.Controls.Add(this.lblPeso);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPeso;
+        private System.Windows.Forms.Label lblAltura;
+        private System.Windows.Forms.TextBox txtPeso;
+        private System.Windows.Forms.TextBox txtAltura;
+        private System.Windows.Forms.Button btnCalculoIMC;
+        private System.Windows.Forms.Label lblIMC;
+        private System.Windows.Forms.TextBox txtIMC;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtClassificacao;
+        private System.Windows.Forms.Label lblClassificacao;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label lblPaciente;
+    }
+}
+
